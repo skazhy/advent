1fb120c3eb73f946d19fac78d69054e2ab56a6eb
Karlis Lauva <k@karlis.me>
2017-12-05 08:44:34 +0200

    add days 2-5

--- src/advent/helpers.clj
+++ src/advent/helpers.clj
@@ -1,10 +1,14 @@
-(ns advent.helpers)
+(ns advent.helpers
+  (:require [clojure.string :refer [split trim]]))
 
+
+(defn neg [x] (* -1 x))
 
 (defn digit-seq
   "Takes number as a string and returns a sequence with all digits as integers."
-  [captcha]
-  (map #(Integer. (str %)) captcha))
+  [int-str]
+  (map #(Integer. (str %)) int-str))
+
 
 (defn reduce-indexed
   [f acc items]
@@ -13,3 +17,19 @@
       (fn [[acc idx] item] [(f idx acc item) (inc idx)])
       [acc 0]
       items)))
+
+
+;;; I/O
+
+(defn slurp-lines [path]
+  (-> (slurp path) (split #"\n")))
+
+(defn slurp-int-lines [path]
+  (map #(Integer. %) (slurp-lines path)))
+
+(defn slurp-word-lines [path]
+  (map #(split (trim %) #" ") (slurp-lines path)))
+
+(defn slurp-int-matrix [path]
+  (map (fn [line] (map  #(Integer. %) (split line #" ")))
+       (slurp-lines path)))
