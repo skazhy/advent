0a5077ac131126b016cfa64c2f01d18207eae41f
Karlis Lauva <k@karlis.me>
2017-12-08 10:27:51 +0200

    day8: naming

--- src/advent/2017/day8.clj
+++ src/advent/2017/day8.clj
@@ -8,29 +8,29 @@
 
 (defn- parse-instruction [row]
   ; format: [foo inc 10 if bar > 10]
-  (when-let [[dest-reg upd-op upd-with _ src-reg comp-op comp-with] row]
-    {:dest-reg dest-reg
-     :compare (instruciton-fn (get comp-ops comp-op) comp-with)
-     :update (instruciton-fn (get upd-ops upd-op) upd-with)
-     :src-reg src-reg}))
+  (when-let [[register upd-op upd-with _ compare-reg comp-op comp-with] row]
+    {:register register
+     :compare-fn (instruciton-fn (get comp-ops comp-op) comp-with)
+     :update-fn (instruciton-fn (get upd-ops upd-op) upd-with)
+     :compare-reg compare-reg}))
 
 (defn- can-exec? [registers instruction]
-  ((:compare instruction) (get registers (:src-reg instruction))))
+  ((:compare-fn instruction) (get registers (:compare-reg instruction))))
 
 (defn- apply-instructions [rows extract-result]
   (loop [rows rows registers {} max-value 0]
     (if-let [instruction (parse-instruction (first rows))]
       (if (can-exec? registers instruction)
-        (let [res ((:update instruction)
-                    (get registers (:dest-reg instruction)))]
+        (let [res ((:update-fn instruction)
+                    (get registers (:register instruction)))]
           (recur (rest rows)
-                 (assoc registers (:dest-reg instruction) res)
+                 (assoc registers (:register instruction) res)
                  (max max-value res)))
         (recur (rest rows) registers max-value))
       (extract-result registers max-value))))
 
 (defn puzzle1 [rows]
-  (apply-instructions rows (fn [regs _] (apply max (map last regs)))))
+  (apply-instructions rows (fn [registers _] (apply max (map last registers)))))
 
 (defn puzzle2 [rows]
   (apply-instructions rows (fn [_ max-value] max-value)))
