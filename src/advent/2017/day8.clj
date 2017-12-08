962102b99bdaf1ff308bbcd41b7d385875535a59
Karlis Lauva <k@karlis.me>
2017-12-08 11:38:44 +0200

    day8: simplify

--- src/advent/2017/day8.clj
+++ src/advent/2017/day8.clj
@@ -6,23 +6,21 @@
 
 (defn- instruciton-fn [op arg] (fn [i] (op (or i 0) (Integer. arg))))
 
+(defn- register-fn [register op arg]
+  (fn [registers] ((instruciton-fn op arg) (get registers register))))
+
 (defn- parse-instruction [row]
   ; format: [foo inc 10 if bar > 10]
   (when-let [[register upd-op upd-with _ compare-reg comp-op comp-with] row]
     {:register register
-     :compare-fn (instruciton-fn (get comp-ops comp-op) comp-with)
-     :update-fn (instruciton-fn (get upd-ops upd-op) upd-with)
-     :compare-reg compare-reg}))
-
-(defn- can-exec? [registers instruction]
-  ((:compare-fn instruction) (get registers (:compare-reg instruction))))
+     :compare-fn (register-fn compare-reg (get comp-ops comp-op) comp-with)
+     :update-fn (register-fn register (get upd-ops upd-op) upd-with)}))
 
 (defn- apply-instructions [rows extract-result]
   (loop [rows rows registers {} max-value 0]
     (if-let [instruction (parse-instruction (first rows))]
-      (if (can-exec? registers instruction)
-        (let [res ((:update-fn instruction)
-                    (get registers (:register instruction)))]
+      (if ((:compare-fn instruction) registers)
+        (let [res ((:update-fn instruction) registers)]
           (recur (rest rows)
                  (assoc registers (:register instruction) res)
                  (max max-value res)))
