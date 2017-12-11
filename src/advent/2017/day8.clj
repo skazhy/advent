bf248608d0c8b91e4eae2c005b74c60b35071482
Karlis Lauva <k@karlis.me>
2017-12-11 12:53:03 +0200

    day8: typofix

--- src/advent/2017/day8.clj
+++ src/advent/2017/day8.clj
@@ -1,13 +1,14 @@
-(ns advent.2017.day8)
+(ns advent.2017.day8
+  "Advent of Code 2017, day 8: Register instruction parsing and execution")
 
 
 (def ^:private comp-ops {"!=" not= "<=" <= "==" = ">=" >= ">" > "<" <})
 (def ^:private upd-ops {"inc" + "dec" -})
 
-(defn- instruciton-fn [op arg] (fn [i] (op (or i 0) (Integer. arg))))
+(defn- instruction-fn [op arg] (fn [i] (op (or i 0) (Integer. arg))))
 
 (defn- register-fn [register op arg]
-  (fn [registers] ((instruciton-fn op arg) (get registers register))))
+  (fn [registers] ((instruction-fn op arg) (get registers register))))
 
 (defn- parse-instruction [row]
   ; format: [foo inc 10 if bar > 10]
