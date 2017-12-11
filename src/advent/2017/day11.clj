efce16ce2bae26c789a6a1b35eca7aba459a4cc5
Karlis Lauva <k@karlis.me>
2017-12-11 12:59:13 +0200

    add day11

---
+++ src/advent/2017/day11.clj
@@ -0,0 +1,42 @@
+(ns advent.2017.day11)
+
+
+;;; Coordinate system
+;;; all odd x columns are "half step" higher than even x columns.
+;;;
+;;; < 0  2 >----< 2  2 >----<
+;;;  >----< 1  1 >----< 3  1 >
+;;; < 0  1 >----< 2  1 >----<
+;;;  >----< 1  0 >----< 3  0 >
+;;; < 0  0 >----< 2  0 >----<
+;;;  >----< 1 -1 >----< 3 -1 >
+;;; < 0 -1 >----< 2 -1 >----<
+
+(def ^:private odd-x-direction
+  {"n" [0 1] "s" [0 -1] "ne" [1 1] "nw" [-1 1] "sw" [-1 0] "se" [1 0]})
+
+(def ^:private even-x-direction
+  {"n" [0 1] "s" [0 -1] "ne" [1 0] "nw" [-1 0] "sw" [-1 -1] "se" [1 -1]})
+
+(defn coord [loc direction]
+  (if (even? (first loc))
+    (get even-x-direction direction)
+    (get odd-x-direction direction)))
+
+(defn steps-to-origin [point]
+  (let [[x y] (map #(Math/abs %) c)]
+    (+ x (max (- y x) 0) (int (Math/ceil (/ x 2))))))
+
+(defn navigate-home [steps callback-fn]
+  (loop [steps steps loc [0 0] furthest 0]
+    (if-let [x (coord loc (first steps))]
+      (recur (rest steps) (map + loc x) (max furthest (steps-to-origin loc)))
+      (callback-fn (steps-to-origin loc) furthest))))
+
+(defn- parse-input [input] (clojure.string/split input #","))
+
+(defn puzzle1 [input]
+  (navigate-home (parse-input input) (fn [step-count _] step-count)))
+
+(defn puzzle2 [input]
+  (navigate-home (parse-input input) (fn [_ max-distance] max-distance)))
