b50ab1a347c15bb4529c0c3bcb5f192b93f9085b
Karlis Lauva <k@karlis.me>
2018-12-03 11:06:25 +0200

    day3: more tweaks

--- src/advent/2018/day3.clj
+++ src/advent/2018/day3.clj
@@ -2,6 +2,7 @@
   "Advent of Code 2018, day 3: No Matter How You Slice It"
   (:require [advent.helpers :as h]))
 
+; patch id - horizontal offset - vertical offset - width - height
 (def row-pattern #"#(\d+) @ (\d+).(\d+): (\d+)x(\d+)")
 
 (def puzzle-input (h/slurp-resource "2018/day3.txt" h/slurp-lines))
@@ -9,26 +10,26 @@
 (defn- parse-row [row]
   (->> (re-matches row-pattern row) rest (map #(Integer. %))))
 
+(defn- offset-range [offset v] (range offset (+ offset v)))
+
 (defn rect-range [[_ left-offset top-offset x y]]
-  (for [x (range left-offset (+ left-offset x))
-        y (range top-offset (+ top-offset y))]
-    [x y]))
+  (for [x (offset-range left-offset x) y (offset-range top-offset y)] [x y]))
+
+;;;
 
 (defn puzzle1 [input]
-  (loop [grid {} rows (map parse-row input)]
-    (if (seq rows)
-      (recur
-        ; Store number of overlaps in the grid map value.
-        (merge-with + grid (zipmap (rect-range (first rows)) (repeat 1)))
-        (rest rows))
-      (reduce-kv (fn [acc _ v] (if (< 1 v) (inc acc) acc)) 0 grid))))
+  (->> (map parse-row input)
+       (reduce #(merge-with + %1 (zipmap (rect-range %2) (repeat 1))) {})
+       (reduce-kv (fn [acc _ v] (if (< 1 v) (inc acc) acc)) 0)))
+
+;;;
 
 (defn puzzle2 [input]
   (loop [grid {} non-overlapping #{} rows (map parse-row input)]
     (if (seq rows)
       (let [rng (rect-range (first rows))
             id (ffirst rows)
-            overlaps-with (distinct (keep #(get grid %) rng))]
+            overlaps-with (distinct (keep grid rng))]
         (recur
           ; Store the patch ID in the grid value, so we can figure out
           ; if there are overlap conflicts later.
