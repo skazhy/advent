c3f2689f1d533e75f96eb0130e08941c7c68a3a9
Karlis Lauva <k@karlis.me>
2019-12-03 06:27:15 +0200

    2019 day2: move formatter to shared helpers

--- src/advent/2019/day2.clj
+++ src/advent/2019/day2.clj
@@ -1,18 +1,15 @@
 (ns advent.2019.day2
   "Advent of Code 2019, day 2: 1202 Program Alarm"
-  (:require [clojure.string :as str]
-            [advent.helpers :as h]))
+  (:require [advent.helpers :as h]))
 
-(def puzzle-input (h/slurp-resource "2019/day2.txt" h/slurp-line))
-
-(defn parse-input [input] (mapv #(Integer/parseInt %) (str/split input #",")))
+(def puzzle-input (h/slurp-resource "2019/day2.txt" h/slurp-int-csv-line))
 
 (defn valid-op? [input offset]
   (let [op (nth input offset)]
     (when-not (= 99 op) op)))
 
 (defn run-program [input noun verb]
-  (loop [input (assoc input 1 noun 2 verb) offset 0]
+  (loop [input (assoc (vec input) 1 noun 2 verb) offset 0]
     (if-let [op (valid-op? input offset)]
       (let [[a b res] (subvec input (inc offset) (+ 4 offset))
             op (case op 1 + 2 *)]
@@ -20,14 +17,13 @@
                (+ offset 4)))
       (first input))))
 
-(defn puzzle1 [input] (run-program (parse-input input) 12 2))
+(defn puzzle1 [input] (run-program input 12 2))
 
 (def ^:private puzzle2-output 19690720)
 
 (defn puzzle2 [input]
-  (let [input (parse-input input)
+  (let [base (run-program input 0 0)
         ; program is a linear function. "verb" argument delta is always 1.
-        base (run-program input 0 0)
         noun-delta (- (run-program input 1 0) base)
         noun (int (/ (- puzzle2-output base) noun-delta))]
     (+ (* 100 noun)
