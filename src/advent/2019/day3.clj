f16fcfb6b8952db07358293244db05a7583c8c37
Karlis Lauva <k@karlis.me>
2019-12-03 20:41:59 +0100

    2019 day 3: simplify

--- src/advent/2019/day3.clj
+++ src/advent/2019/day3.clj
@@ -1,12 +1,10 @@
 (ns advent.2019.day3
   "Advent of Code 2019, day 3: Crossed Wires"
-  (:require [clojure.set :as set]
-            [clojure.string :as str]
-            [advent.helpers :as h]))
+  (:require [advent.helpers :as h]))
 
 (def puzzle-input (h/slurp-resource "2019/day3.txt" h/slurp-csv-lines))
 
-(defn parse-wire-segment [s]
+(defn parse-wire-input [s]
   [(first s) (Integer/parseInt (.substring s 1))])
 
 (def directions
@@ -15,38 +13,41 @@
    \U [0 1]
    \D [0 -1]})
 
-(defn expand-wire [wire]
-  (loop [wire wire expanded [(list 0 0)]]
-    (if-let [[dir len] (some-> (first wire) parse-wire-segment)]
-      (let [move (get directions dir)]
-        (recur
-         (rest wire)
-         (->> (range len)
-              (reductions (fn [acc _] (map + acc move)) (last expanded))
-              (rest)  ; remove duplications on wire turns
-              (into expanded))))
-      (rest expanded))))  ; remove [0 0] coordinate
+;; wire is a vector containing all coordinates a wire passes
+;; eg: [ [1 0] [2 0] [2 1] ...]
 
-(defn manhattan-distance [[x y]]
+(defn mk-wire [input]
+  (loop [input input wire [(list 0 0)]]
+    (if-let [[dir len] (some-> (first input) parse-wire-input)]
+      (let [direction (get directions dir)]
+        (recur
+         (rest input)
+         (->> (range len)
+              (reductions (fn [acc _] (map + acc direction)) (last wire))
+              (rest)  ; remove duplications on wire turns
+              (into wire))))
+      (rest wire))))  ; remove the initial [0 0] coordinate
+
+(defn manhattan-distance
+  "Returns Manhattan distance to a given coordinate"
+  [[x y]]
   (+ (Math/abs x) (Math/abs y)))
 
-(defn expanded-wire-intersections [expanded-wire-a expanded-wire-b]
-  (set/intersection (set expanded-wire-a) (set expanded-wire-b)))
-
-(defn minimal-by [cmp [h & tail]]
+(defn minimal-by
   "Finds the minimal mapped value for the collection."
+  [cmp [h & tail]]
   (reduce (fn [acc i] (min acc (cmp i))) (cmp h) tail))
 
 (defn puzzle1
   "Returns the closest intersection for two given wires"
-  [[wire-a wire-b]]
-  (->> (expanded-wire-intersections (expand-wire wire-a) (expand-wire wire-b))
+  [[input-a input-b]]
+  ; Drop [0 0] since it doesn't count as an intersection.
+  (->> (h/intersection (rest (mk-wire input-a)) (rest (mk-wire input-b)))
        (minimal-by manhattan-distance)))
 
-(defn puzzle2 [[wire-a wire-b]]
-  (let [expanded-a (expand-wire wire-a)
-        expanded-b (expand-wire wire-b)]
-    (->> (expanded-wire-intersections (set expanded-a)
-                                      (set expanded-b))
-         ; + 1 for each initial move, since [0 0] was dropped in expand-wire
-         (minimal-by #(+ 2 (.indexOf expanded-a %) (.indexOf expanded-b %))))))
+(defn puzzle2 [[input-a input-b]]
+  (let [wire-a (mk-wire input-a)
+        wire-b (mk-wire input-b)
+        ; + 1 for each removed [0 0] coordinate
+        combined-distance #(+ 2 (.indexOf wire-a %) (.indexOf wire-b %))]
+    (minimal-by combined-distance (h/intersection wire-a wire-b))))
