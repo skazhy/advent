5af09e4b28710c53183e07c9214fdc3c60a52a24
Karlis Lauva <k@karlis.me>
2019-12-05 20:32:43 +0100

    2019 day 5

--- src/advent/2019/day2.clj
+++ src/advent/2019/day2.clj
@@ -1,30 +1,26 @@
 (ns advent.2019.day2
   "Advent of Code 2019, day 2: 1202 Program Alarm"
-  (:require [advent.helpers :as h]))
+  (:require [advent.helpers :as h]
+            [advent.helpers.intcode :refer [run-program]]))
 
-(def puzzle-input (h/slurp-resource "2019/day2.txt" h/slurp-int-csv-line))
+(def puzzle-input
+  (h/slurp-resource "2019/day2.txt" (comp vec h/slurp-int-csv-line)))
 
-(defn valid-op? [input offset]
-  (let [op (nth input offset)]
-    (when-not (= 99 op) op)))
+;;; All interesting bits for this puzzle are in the incode implementation.
 
-(defn run-program [input noun verb]
-  (loop [input (assoc (vec input) 1 noun 2 verb) offset 0]
-    (if-let [op (valid-op? input offset)]
-      (let [[a b res] (subvec input (inc offset) (+ 4 offset))
-            op (case op 1 + 2 *)]
-        (recur (assoc input res (op (nth input a) (nth input b)))
-               (+ offset 4)))
-      (first input))))
+(defn run* [input noun verb]
+  (-> (assoc input 1 noun 2 verb)
+      (run-program [])
+      (:program) first))
 
-(defn puzzle1 [input] (run-program input 12 2))
+(defn puzzle1 [input] (run* input 12 2))
 
 (def ^:private puzzle2-output 19690720)
 
 (defn puzzle2 [input]
-  (let [base (run-program input 0 0)
+  (let [base (run* input 0 0)
         ; program is a linear function. "verb" argument delta is always 1.
-        noun-delta (- (run-program input 1 0) base)
+        noun-delta (- (run* input 1 0) base)
         noun (int (/ (- puzzle2-output base) noun-delta))]
     (+ (* 100 noun)
        (- puzzle2-output base (* noun noun-delta)))))
---
+++ src/advent/helpers/intcode.clj
@@ -0,0 +1,63 @@
+(ns advent.helpers.intcode
+  "Intcode helpers (for 2019 puzzles)")
+
+(defn parse-opcode
+  "Takes an intcode and returns it split to digits with the opcode
+   in the first position."
+  [{:keys [program offset]}]
+  (let [raw-op (nth program offset)
+        opcode (rem raw-op 100)]
+    [opcode (map #(Integer/parseInt (str %)) (str (int (/ raw-op 100))))]))
+
+(defn padded-modes [modes t immediate-last?]
+  (let [modes (concat (repeat (- t (count modes)) 0) modes)]
+    (if immediate-last?
+      (conj (rest modes) 1)
+      modes)))
+
+(defn prepare-args [{:keys [program offset]} modes n immediate-last?]
+  (map (fn [mode arg] (if (zero? mode) (nth program arg) arg))
+       (reverse (padded-modes modes n immediate-last?))
+       (subvec program (inc offset) (+ 1 n offset))))
+
+(defn run-command [arg-count last-arg-immediate? op-fn]
+  (fn [program modes]
+    (-> (update program :offset + 1 arg-count)  ; number of args + opcode
+        (op-fn (prepare-args program modes arg-count last-arg-immediate?)))))
+
+(defn simple-op [op]
+  (run-command 3 true (fn [p [a b res]] (assoc-in p [:program res] (op a b)))))
+
+(def input-op
+  (run-command 1 true (fn [p [res]] (assoc-in p [:program res] (:input p)))))
+
+(def output-op
+  (run-command 1 false (fn [p [o]] (update p :outputs conj o))))
+
+(defn cond-jump-op [pred]
+  (run-command 2 false (fn [p [t res]] (if (pred t) (assoc p :offset res) p))))
+
+(defn compare-op [cmp]
+  (run-command 3 true (fn [p [a b res]]
+                        (assoc-in p [:program res] (if (cmp a b) 1 0)))))
+
+(def halt-op (run-command 0 false (fn [p _] (assoc p :halt true))))
+
+(def operations
+  {1 (simple-op +)
+   2 (simple-op *)
+   3 input-op
+   4 output-op
+   5 (cond-jump-op (complement zero?))
+   6 (cond-jump-op zero?)
+   7 (compare-op <)
+   8 (compare-op =)
+   99 halt-op})
+
+(defn run-program
+  "Runs the intcode computer with the given input and a map of known commands."
+  [program input]
+  (loop [program {:program program :offset 0 :outputs [] :input input}]
+    (let [[op modes] (parse-opcode program)
+          program ((get operations op) program modes)]
+      (if (:halt program) program (recur program)))))
