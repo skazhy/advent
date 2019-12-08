d3e21f226ae6b7495d384ec9c3527e68c6616ee5
Karlis Lauva <k@karlis.me>
2019-12-08 22:13:01 +0200

    2019 day 8

---
+++ src/advent/2019/day8.clj
@@ -0,0 +1,35 @@
+(ns advent.2019.day8
+  "Advent of Code 2019, day 8: Space Image Format"
+  (:require [clojure.string :as str]
+            [advent.helpers :as h]))
+
+(def w 25)
+(def h 6)
+
+(def puzzle-input (h/slurp-resource "2019/day8.txt" h/slurp-line))
+
+(defn image-layers [input w h]
+  (partition-all (* w h) input))
+
+(defn puzzle1 [input]
+  (let [freqs (->> (image-layers input w h)
+                   (sort-by (comp count (partial filter #(= \0 %))))
+                   (first)
+                   (frequencies))]
+    (*  (get freqs \1) (get freqs \2))))
+
+;;;
+
+(defn transparent? [pixel] (= \2 pixel))
+
+(defn compose-pixels [pixels] (or (first (remove transparent? pixels)) \2))
+
+(defn compose-layers [layers]
+  (apply map (fn [& pixels] (compose-pixels pixels)) layers))
+
+(defn format-layer [layer w]
+  (->> (partition w (map #(if (= % \0) " " "X") layer))
+       (map #(apply str %))))
+
+(defn puzzle2 [input]
+  (format-layer (compose-layers (image-layers input w h)) w))
---
+++ test/advent/2019/test_day8.clj
@@ -0,0 +1,15 @@
+ (ns advent.2019.test-day8
+  (:require [clojure.test :refer :all]
+            [advent.2019.day8 :as d]))
+
+(deftest puzzle1
+  (is (= 2440 (d/puzzle1 d/puzzle-input))))
+
+(deftest puzzle2
+  (is (= [" XX  XXXX  XX    XX  XX  "
+          "X  X    X X  X    X X  X "
+          "X  X   X  X       X X    "
+          "XXXX  X   X       X X    "
+          "X  X X    X  X X  X X  X "
+          "X  X XXXX  XX   XX   XX  "]
+         (d/puzzle2 d/puzzle-input))))
