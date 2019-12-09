6125d9acea0c03535de4d3856dfba5e8280db500
Karlis Lauva <k@karlis.me>
2019-12-09 23:02:25 +0200

    2019 day 7: part two

--- test/advent/2019/test_day7.clj
+++ test/advent/2019/test_day7.clj
@@ -16,9 +16,15 @@
   (testing "Actual input"
     (is (= 34852 (d/puzzle1 d/puzzle-input)))))
 
-#_ (deftest ^:intcode puzzle2
-    (testing "Examples"
-      (is (= 42 (d/puzzle2 "test"))))
+(deftest ^:intcode puzzle2
+  (testing "Examples"
+    (is (= 139629729 (d/puzzle2 [3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26
+                                 27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5])))
 
-    (testing "Actual input"
-      (is (= 42 (d/puzzle2 d/puzzle-input)))))
+    (is (= 18216 (d/puzzle2 [3 52 1001 52 -5 52 3 53 1 52 56 54 1007 54 5
+                             55 1005 55 26 1001 54 -5 54 1105 1 12 1 53 54
+                             53 1008 54 0 55 1001 55 1 55 2 53 55 53 4 53
+                             1001 56 -1 56 1005 56 6 99 0 0 0 0 10]))))
+
+  (testing "Actual input"
+    (is (= 44282086 (d/puzzle2 d/puzzle-input)))))
