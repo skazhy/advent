2bfd3238306adb560aa704ff64be44044a092fa8
Karlis Lauva <k@karlis.me>
2021-12-10 09:27:33 +0200

    small tweaks for 2021.10

--- src/haskell/2021/Day10.hs
+++ src/haskell/2021/Day10.hs
@@ -11,14 +11,10 @@ import Advent
 import Data.List (sort)
 import Data.Maybe (mapMaybe)
 import Data.Map (Map, fromList, lookup, member, (!))
-import Data.Tuple (swap)
 
 data Error = Corrupted Char | Incomplete String deriving (Show)
 
-brackets = [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]
-
-bracketMap = fromList brackets
-inverseBracketMap = fromList $ map swap brackets
+bracketMap = fromList [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]
 
 matchingBrackets :: Char -> Char -> Bool
 matchingBrackets open close =
@@ -28,7 +24,7 @@ lineError :: String -> Maybe Error
 lineError = go [] where
   go (x:xs) (y:ys)
     | matchingBrackets x y = go xs ys
-    | Data.Map.member y inverseBracketMap = Just (Corrupted y)
+    | not $ Data.Map.member y bracketMap = Just (Corrupted y)
     | otherwise = go (y:x:xs) ys
   go [] [] = Nothing
   go [] (x:xs)
@@ -43,7 +39,7 @@ incompleteScores = fromList [('(', 1), ('[', 2), ('{', 3), ('<', 4)]
 
 scoreError :: Error -> Int
 scoreError (Corrupted a) = corruptedScores ! a
-scoreError (Incomplete a) = foldl (\acc i -> (acc * 5) + i) 0 $ map (incompleteScores !) a
+scoreError (Incomplete a) = foldl1 (\acc i -> (acc * 5) + i) $ map (incompleteScores !) a
 
 isCorrupted :: Error -> Bool
 isCorrupted (Corrupted _) = True
