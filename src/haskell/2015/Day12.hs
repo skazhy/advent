2b80a2878703e7e55db7fd6fda14e3bb923b164e
Karlis Lauva <k@karlis.me>
2023-11-09 17:50:50 +0200

    2015.12.1

---
+++ src/haskell/2015/Day12.hs
@@ -0,0 +1,27 @@
+{-|
+Module      : Day12
+Description : Day 12: JSAbacusFramework.io
+
+<https://adventofcode.com/2015/day/12>
+-}
+
+module Day12 where
+
+import Advent
+
+import Data.Char (ord)
+import Data.List (takeWhile, dropWhile, isSubsequenceOf)
+
+numChar :: Char -> Bool
+numChar c = ord c > 47 && ord c < 58 || ord c == 45
+
+parseChunk :: String -> Int
+parseChunk s =
+  go s 0 where
+  go [] n = n
+  go (s:sx) n | numChar s = go (dropWhile numChar sx) $ n + read (s : takeWhile numChar sx)
+  go (_:sx) n = go sx n
+
+main = do
+    input <- parsedInput (2015, 12) lines
+    print $ sum $ map parseChunk input
