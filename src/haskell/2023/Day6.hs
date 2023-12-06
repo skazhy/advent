aa54eabd4886e96a60269a70d432a926d389d0ab
Karlis Lauva <k@karlis.me>
2023-12-06 09:50:56 +0200

    2023.6 in Haskell

---
+++ src/haskell/2023/Day6.hs
@@ -0,0 +1,40 @@
+{-|
+Module      : Day6
+Description : Day 6: Wait For It
+
+<https://adventofcode.com/2023/day/6>
+-}
+
+module Day6 where
+
+import Advent
+import Data.List (transpose)
+import Data.Maybe (mapMaybe)
+
+toTuple :: [a] -> Maybe (a, a)
+toTuple (x:y:_) = Just (x, y)
+toTuple _ = Nothing
+
+distances :: (Int, Int) -> Int
+distances (t,r) = length $ takeWhile (> r) $ dropWhile (< r) $ map (\h -> h * (t - h)) [1..t]
+
+-- Part 1
+-- Parsing data as separate columns and finding `distances` for each
+
+parseColumns :: [String] -> [(Int, Int)]
+parseColumns = mapMaybe toTuple . transpose . map (map read . tail . words)
+
+-- Part 2
+-- Treating input as single (time, maxDistance) tuple
+
+parseRow :: String -> Int
+parseRow = read . concat . tail . words
+
+parseRows :: [String] -> Maybe (Int, Int)
+parseRows (x:y:_) = Just (parseRow x, parseRow y)
+parseRows _ = Nothing
+
+main = do
+    input <- parsedInput (2023, 6) lines
+    print $ product $ map distances $ parseColumns input
+    printMaybe $ distances <$> parseRows input
